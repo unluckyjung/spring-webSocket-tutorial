180e737f11cf85dc877ff3fddffef9c57b7d006c
unluckyjung <ybook2012@gmail.com>
2021-08-18 01:16:55 +0900

    chore: init

---
+++ web-socket/src/main/java/com/example/simplewebsocket/SimpleWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplewebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebSocketApplication.class, args);
+	}
+
+}
---
+++ web-socket/src/test/java/com/example/simplewebsocket/SimpleWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplewebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
